7c655086fb1ec91a9332c0009822b6d4182b98f8
Yoshida Shin <wbcchsyn@gmail.com>
2020-09-04 13:48:36 +0900

    Declare struct MemoryBlock
    MemoryBlock was in API.
    Now that it is removed, Declare it by this crate itself.

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,6 @@ mod ptr_list;
 pub use crate::bulk_allocator::BulkAllocator;
 pub use crate::layout_bulk_allocator::LayoutBulkAllocator;
 use crate::ptr_list::PtrList;
-use core::alloc::MemoryBlock;
 use core::mem::size_of;
 use core::ptr::NonNull;
 
@@ -54,6 +53,12 @@ const MIN_CACHE_SIZE: usize = size_of::<PtrList>();
 // This must equal to 2 * MAX_CACHE_SIZE or larger; otherwise BulkAllocator
 // doesn't always make cache for MAX_CACHE_SIZE.
 const MEMORY_CHUNK_SIZE: usize = 8 * MAX_CACHE_SIZE;
+
+#[derive(Clone, Copy)]
+struct MemoryBlock {
+    ptr: NonNull<u8>,
+    size: usize,
+}
 
 fn split_memory_block(block: MemoryBlock, count: usize) -> (MemoryBlock, MemoryBlock) {
     debug_assert!(count <= block.size);
