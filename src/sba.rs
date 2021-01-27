34a66bd18245d95d8c48756bad07f2aa4d7d9699
Yoshida Shin <wbcchsyn@gmail.com>
2021-01-27 09:37:39 +0900

    Create struct sba::Cache

--- src/sba.rs
+++ src/sba.rs
@@ -28,3 +28,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use crate::PtrList;
+
+/// Structure for `Sba` and `Usba` .
+struct Cache {
+    to_free: PtrList,
+    pool: PtrList,
+}
