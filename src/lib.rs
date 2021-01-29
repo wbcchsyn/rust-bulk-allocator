da6b7b6b29cd7abe61e8e82718d507f9f246cece
Yoshida Shin <wbcchsyn@gmail.com>
2021-01-29 09:30:00 +0900

    Update crate document

--- src/lib.rs
+++ src/lib.rs
@@ -31,36 +31,16 @@
 
 #![deny(missing_docs)]
 
-//! bulk-allocator is implementations for AllocRef.
-//! They pool allocated memory and release them on the destruction.
+//! bulk-allocator is implementations for GlobalAlloc holding memory cache.
+//! The instance acquires bulk memories from the backend, and frees them on the drop at once for
+//! the performance.
 //!
-//! Method `dealloc` caches the released memory and `alloc` reuses it.
-//! If no cache is left, `alloc` acquires a memory chunk from the backend and make cache at first.
+//! Method `dealloc` does not free the specified pointer soon, but pools in the cache.
 //!
-//! Using cache effectively, the performance is better compared to the same name functions in `std::alloc`.
+//! Method `alloc` pops and returns from the cache if not empty; otherwise, `alloc` allocates a
+//! bulk memory from the backend, splits into pieces to make cache at first.
 //!
-//! # Note
-//!
-//! Trait `AllocRef` is defined only in Rust nightly toolchain so far.
-//! It is required to enable feathre `allocator_api` to use this crate and `AllocRef`.
-//!
-//! # Implementations
-//!
-//! ## BulkAllocator
-//!
-//! `AllocRef` method `alloc` and `dealloc` delegate the request to the backend if the argument `layout`
-//! is too large to cache; otherwise, `dealloc` stores the released memory and `alloc` dispatches it and return.
-//!
-//! If the argument `layout` is always same, probably `LayoutBulkAllocator` is better.
-//!
-//! All methods in `AllocRef` are thread unsafe.
-//!
-//! ## LayoutBulkAllocator
-//!
-//! `LayoutBulkAllocator` behaves like `BulkAllocator` except for the constructor requires `Layout` as the argument.
-//!
-//! The instance uses cache only when the argument `layout` is same to what the constructor is passed; otherwise,
-//! the requests are delegated to the backend.
+//! It is when the instance is dropped that the memory chunks are deallocated.
 
 mod bulk_a;
 mod layout_bulk_a;
