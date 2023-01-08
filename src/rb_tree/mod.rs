e73036d216dd4006ebf6388ea5d4b48ca113ddc2
Yoshida Shin <wbcchsyn@gmail.com>
2023-01-08 16:26:24 +0900

    Create enum rb_tree::Color

--- src/rb_tree/mod.rs
+++ src/rb_tree/mod.rs
@@ -28,3 +28,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    Red,
+    Black,
+}
