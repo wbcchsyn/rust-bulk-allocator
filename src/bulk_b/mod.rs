f86459a0d9d8d54c29ba9924977004dc05089db2
Yoshida Shin <wbcchsyn@gmail.com>
2023-01-25 14:02:22 +0900

    Create module bulk_b

--- src/bulk_b/mod.rs
+++ src/bulk_b/mod.rs
@@ -28,33 +28,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![deny(missing_docs)]
-
-//! bulk-allocator is implementations for GlobalAlloc holding memory cache.
-//! The instance acquires bulk memories from the backend, and frees them on the drop at once for
-//! the performance.
-//!
-//! Method `dealloc` does not free the specified pointer soon, but pools in the cache.
-//!
-//! Method `alloc` pops and returns from the cache if not empty; otherwise, `alloc` allocates a
-//! bulk memory from the backend, splits into pieces to make cache at first.
-//!
-//! It is when the instance is dropped that the memory chunks are deallocated.
-
-mod bulk_a;
-mod bulk_b;
-mod layout_bulk_a;
-mod layout_bulk_b;
-mod ptr_list;
-mod rb_tree;
-
-pub use bulk_a::{BulkA, UnBulkA};
-pub use layout_bulk_a::{LayoutBulkA, UnLayoutBulkA};
-pub use layout_bulk_b::{LayoutBulkAlloc, UnsafeLayoutBulkAlloc};
-use ptr_list::PtrList;
-
-/// The default byte count of bulk memory that this crate allocates from the backend if no cache
-/// is.
-/// Note that if too large layout is requested, the bulk size may exceed this value.
-pub const MEMORY_CHUNK_SIZE: usize = 16384; // 16 KB
