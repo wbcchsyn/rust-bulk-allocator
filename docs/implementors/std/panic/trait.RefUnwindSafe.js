acecf7b459a7222007f8f774178695f61b8b4663
Yoshida Shin <wbcchsyn@gmail.com>
2023-02-25 16:16:13 +0900

    Delete directory "docs"

--- docs/implementors/std/panic/trait.RefUnwindSafe.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["bulk_allocator"] = [{"text":"impl&lt;B&gt; !RefUnwindSafe for BulkA&lt;B&gt;","synthetic":true,"types":[]},{"text":"impl&lt;B&gt; !RefUnwindSafe for UnBulkA&lt;B&gt;","synthetic":true,"types":[]},{"text":"impl&lt;B&gt; !RefUnwindSafe for LayoutBulkA&lt;B&gt;","synthetic":true,"types":[]},{"text":"impl&lt;B&gt; !RefUnwindSafe for UnLayoutBulkA&lt;B&gt;","synthetic":true,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["bulk_allocator"] = {"name":"","files":["bulk_a.rs","layout_bulk_a.rs","lib.rs","ptr_list.rs"]};
-createSourceSidebar();
