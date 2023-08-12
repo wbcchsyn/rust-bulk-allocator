aae1c93a4980d5382fc0764c40f9ece583626ec9
Yoshida Shin <wbcchsyn@gmail.com>
2023-08-12 18:20:55 +0900

    Update the crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,15 @@
 
 #![deny(missing_docs)]
 
-//! bulk-allocator provides implementations of [`GlobalAlloc`] holding memory cache.
-//! The instance acquires memory chunks from the backend and frees them on the drop at once for
+//! `bulk-allocator` provides implementations of [`GlobalAlloc`] that manage a memory cache.
+//! The instance fetches memory chunks from the backend and frees them on the drop at once for
 //! the performance.
 //!
-//! Method `dealloc` does not free the specified pointer immediately, but pools in the cache.
+//! Method `dealloc` does not instantly free the specified pointer; instead, it pools the pointer
+//! within the cache.
 //!
-//! Method `alloc` acquires a memory chunk from the backend and stores into the cache if the cache
-//! is empty, and then pops and returns a pointer from the cache.
+//! Method `alloc` acquires a memory chunk from the backend and fill the cache if necessary, and
+//! returns a requested pointer from the cache.
 //!
 //! It is when the instance is dropped that the memory chunks are deallocated.
 //!
